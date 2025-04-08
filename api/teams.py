cc5f5567290a99e023db37242ded9ee66a1f92da
SJTreadway <treadway08@gmail.com>
2025-04-07 21:05:09 -0500

    updates to o/u data; trying to fix input

--- api/teams.py
+++ api/teams.py
@@ -33,29 +33,44 @@ def get_team_cols(df):
 # create team dataframe to easily aggregate rolling window game data
 def create_team_df(df, team):
   cols = ['AB', 'H', 'x2B', 'x3B', 'HR', 'BB', 'SB', 'CS']
-  for c in cols:
-    df[c] = np.nan
-
-  cols_w_idx = cols + ['date_dblhead']
+  cols_h = ['AB_h', 'H_h', 'x2B_h', 'x3B_h', 'HR_h', 'BB_h', 'SB_h', 'CS_h']
+  cols_v = ['AB_v', 'H_v', 'x2B_v', 'x3B_v', 'HR_v', 'BB_v', 'SB_v', 'CS_v']
 
   df_team_v = df[(df.team_v == team)]
   opp = df_team_v['team_h']
-  df_team_v = df_team_v[cols_w_idx]
+  df_team_v = df_team_v[cols_v + ['date_dblhead']]
+  df_team_v.rename(columns={
+    'AB_v': 'AB', 
+    'H_v': 'H',
+    'x2B_v': 'x2B',
+    'x3B_v': 'x3B',
+    'HR_v': 'HR',
+    'BB_v': 'BB',
+    'SB_v': 'SB',
+    'CS_v': 'CS'
+  }, inplace=True)
   df_team_v['home_game'] = 0
   df_team_v['opponent'] = opp
   
   df_team_h = df[(df.team_h == team)]
   opp = df_team_h['team_v']
-  df_team_h = df_team_h[cols_w_idx]
+  df_team_h = df_team_h[cols_h + ['date_dblhead']]
+  df_team_h.rename(columns={
+    'AB_h': 'AB', 
+    'H_h': 'H',
+    'x2B_h': 'x2B',
+    'x3B_h': 'x3B',
+    'HR_h': 'HR',
+    'BB_h': 'BB',
+    'SB_h': 'SB',
+    'CS_h': 'CS',
+  }, inplace=True)
   df_team_h['home_game'] = 1
   df_team_h['opponent'] = opp
   
   df_team = df_team_h if not df_team_h.empty else df_team_v
   
   df_team = df_team.set_index('date_dblhead')
-
-  # Create Empty Cols before inserting data
-  df_team = df_team.assign(**{col: None for col in cols})
   
   for winsize in WINDOWS:
     suff = str(winsize)
@@ -70,7 +85,7 @@ def create_team_df(df, team):
                                       2 * df_team['rollsum_x3B_' + suff] + 3 * df_team['rollsum_HR_' + suff]) / (
                                       df_team['rollsum_AB_' + suff])
     df_team['rollsum_OBS_' + suff] = df_team['rollsum_OBP_' + suff] + df_team['rollsum_SLG_' + suff]
-      
+  
   return df_team
 
 def generate_team_window_features(df):
